dfc886c364491a5e4dd29a9fb6539b9c3e5bb625
rd273001 <rd273001@gmail.com>
2024-05-31 07:21:26 +0530

    Modified Loading Indicator

--- src/components/LoadingIndicator.jsx
+++ src/components/LoadingIndicator.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 
-const Spinner = () => {
+const LoadingIndicator = () => {
   return (
-    <div className='flex justify-center items-center'>
-      <div className='animate-spin rounded-full h-8 w-8 border-t-2 border-b-2 border-gray-900'></div>
+    <div className='flex flex-col absolute z-30 top-0 left-0 right-0 bottom-0 m-auto border-2 border-gray-400 py-8 px-12 items-center justify-center h-fit rounded-2xl bg-[radial-gradient(#bbb,#444)] w-fit'>
+      <div className='flex items-center justify-center rounded-full p-4 bg-[conic-gradient(indigo,purple,pink,white)] animate-[spin_200ms_linear_infinite]'>
+        <div className='size-7 rounded-full bg-[radial-gradient(#eee,#444)]'></div>
+      </div>
+      <p>Loading...</p>
     </div>
   );
 };
 
-export default Spinner;
+export default LoadingIndicator;
